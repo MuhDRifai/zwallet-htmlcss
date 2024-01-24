52b031e6b84cad55169d7cd600db33e692e497de
MuhDRifai <muh.d.rifai23@gmail.com>
2024-01-24 21:05:09 +0700

    new featured

--- createPin/script.js
+++ createPin/script.js
@@ -1,3 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var inputs = document.querySelectorAll('.otp-input-wrapper input');
+
+    inputs.forEach(function (input, index) {
+        input.addEventListener('input', function () {
+            moveToNextInput(this);
+        });
+
+        // Fokus ke input berikutnya jika sebelumnya diisi (digunakan saat refresh)
+        if (input.value.length === 1 && inputs[index + 1]) {
+            inputs[index + 1].focus();
+        }
+    });
+
+    document.querySelector('.login').addEventListener('click', function() {
+        // Mengambil nilai token dari input
+        var token =
+            document.getElementById('otp1').value +
+            document.getElementById('otp2').value +
+            document.getElementById('otp3').value +
+            document.getElementById('otp4').value +
+            document.getElementById('otp5').value +
+            document.getElementById('otp6').value;
+
+        // Ganti dengan logika validasi token Anda
+        var isTokenCorrect = validateToken(token);
+
+        // Mengarahkan pengguna ke halaman yang sesuai
+        if (isTokenCorrect) {
+            window.location.href = 'success.html';
+        } else {
+            window.location.href = 'failed.html';
+        }
+    });
 
     function moveToNextInput(currentInput) {
         var nextInput = currentInput.nextElementSibling;
@@ -8,22 +42,9 @@
         }
     }
 
-    document.addEventListener('DOMContentLoaded', function () {
-        var inputs = document.querySelectorAll('.otp-input-wrapper input');
-
-        inputs.forEach(function (input, index) {
-            input.addEventListener('input', function () {
-                moveToNextInput(this);
-            });
-
-            // Fokus ke input berikutnya jika sebelumnya diisi (digunakan saat refresh)
-            if (input.value.length === 1 && inputs[index + 1]) {
-                inputs[index + 1].focus();
-            }
-        });
-    });
-
-
-    function redirectToLoginPage() {
-        window.location.href = "../login/index.html";
+    function validateToken(token) {
+        var validTokens = ['123456', '654321', '987654']; 
+        // Mengecek apakah token yang dimasukkan ada dalam daftar token yang valid
+        return validTokens.includes(token);
     }
+});
